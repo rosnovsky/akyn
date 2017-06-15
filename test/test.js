c684dd719cbfb0e118304e5b921aec47573623f1
Artem Rosnovskiy <inbox@podtema.com>
2017-06-15 16:00:55 -0700

    Added tests for new routes

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ const mocha = require('mocha');
 const express = require('express');
 const assert = require('assert');
 const app = require('../app.js');
-const readController = require('../controllers/readController');
+const homeController = require('../controllers/homeController');
+const postController = require('../controllers/postController');
 
 // const app = express();
 
@@ -19,18 +20,48 @@ describe('Helpers', function() {
 
 describe('Routes', function() {
 	describe('GET /', function() {
-	  it('responds with 200', function(done) {
+	  it('GET / and responds with 200', function(done) {
 		request(app)
 	      .get('/')
 	      .expect(200, done);
 	  });
 	});
 
-	describe('GET /asdfr', function() {
-	  it('responds with 404', function(done) {
+	describe('GET wrong URL and responds with 404', function() {
+	  it('GET wrong URL and responds with 404', function(done) {
+		const postId = 4201;
 		request(app)
-	      .get('/asdfr')
+				.get(`/${postId}/sdfj`)
 	      .expect(404, done);
 	  });
 	});
+
+	describe('GET /:postId and responds with 200', function() {
+			it('GET /:postId and responds with 200', function(done) {
+				const postId = 4202;
+			request(app)
+					.get(`/${postId}`)
+					.expect(200, done);
+			});
+		});
+
+		describe('GET /:postId/edit and responds with 200', function() {
+			it('GET /:postId/edit and responds with 200', function(done) {
+				const postId = 4202;
+			request(app)
+					.get(`/${postId}/edit`)
+					.expect(200, done);
+			});
+		});
+
+		describe('POST /:postId/delete and responds with 200', function() {
+			it('POST /:postId/delete and responds with 200', function(done) {
+				const postId = 4202;
+			request(app)
+					.post(`/${postId}/delete`)
+					.expect(200, done);
+			});
+		});
+
+
 });
